aa75629dbd8f2b0418a819c6481daf504b9d5dde
elciak82 <elciak82@gmail.com>
2019-10-09 19:26:15 +0200

    zad. 10

---
+++ src/pl/zadaniepetla/ZadanieDziesiate.java
@@ -0,0 +1,30 @@
+package pl.zadaniepetla;
+
+public class ZadanieDziesiate {
+
+    private static int smallNumber = 0;
+    private static int greatNumber = 4;
+    private static String greater = "Podana liczba jest większa od: " + greatNumber;
+    private static String inside = "Podana liczba jest większa od: " + smallNumber + ", ale mniejsza od: " + greatNumber;
+    private static String ouside = "Podana liczba nie mieści się w przedziałach";
+    private static String wrongCondition = "Nieprawidłowy warunek!";
+
+    public static void main(String[] args) {
+        System.out.println(checkNumber(10));
+    }
+
+    private static String checkNumber (int checkedNumber) {
+        if (smallNumber < greatNumber) {
+            if (checkedNumber > greatNumber) {
+                return greater;
+            } else if (smallNumber < checkedNumber && checkedNumber < greatNumber) {
+                return inside;
+            } else return ouside;
+        }else return (wrongCondition);
+    }
+}
+
+//10.	Napisz taki program, który sprawdzi czy zaczytana liczba jest większa od 4
+// i wypisze na konsolę: „Podana liczba jest większa od 4”, jeżeli nie jest,
+// to sprawdzi czy liczba mieści się w przedziale <1 – 4) i wypisze: „Liczba jest mniejsza od 4,
+// ale większa od 1”, jeżeli nie, to program wypisze na konsolę: „Podana liczba nie mieści się w przedziałach”
